37d17d89f25ab39fb56b2658b863dd192ef62ef1
rahulraj77 <120400899+rahulraj77@users.noreply.github.com>
2024-06-24 21:19:03 +0530

    Update 4a.c

--- STM32/4a.c
+++ STM32/4a.c
@@ -1,1 +1,19 @@
-// 4a
+// Interfacing LED TO GPIOS
+#include "main.h"
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Init();
+  MX_GPIO_Init();
+  while(1)
+  {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+    HAL_Delay(1000);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+    HAL_Delay(1000);
+  }
+}
