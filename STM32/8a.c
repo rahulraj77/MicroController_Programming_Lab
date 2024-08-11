0c72b7256c250905b3b1ad24b7739817bc51e4ea
rahulraj77 <120400899+rahulraj77@users.noreply.github.com>
2024-08-11 22:55:31 +0530

    Create 8a.c
    Uploaded 8a

---
+++ STM32/8a.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "accelo.h"
+
+SPI_HandleTypeDef hspi1;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+
+LIS3DSH_DataScaled myData;
+
+int main(void)
+{
+	LIS3DSH_InitTyeDef myaccelo_config;
+  HAL_Init();
+  SystemClock_Config();
+
+	myaccelo_config.dataRate = LIS3DSH_DATARATE_12_5;
+	myaccelo_config.fullScale = LIS3DSH_FULLSCALE_4;
+	myaccelo_config.antiAliasingBW = LIS3DSH_FILTER_BW_50;
+	myaccelo_config.enableAxes = LIS3DSH_XYZ_ENABLE;
+	myaccelo_config.interruptEnable = False;
+
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+	LIS3DSH_Init(&hspi1), &myaccelo_config);
+
+  while (1)
+  {
+		if(LIS3DSH_PollDRDY(1000) == true)
+    {
+			myData = LIS3DSH_GetDataScaled();
+			HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
+		}
+  }
+}
